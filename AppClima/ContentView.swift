25d601bc9e402a185d98eda5663d61cca0f9b1b8
Ignacio <igu99.tsai@gmail.com>
2024-04-18 12:26:22 -0300

    Conceptos basicos sobre propiedades y estructuras

--- AppClima/ContentView.swift
+++ AppClima/ContentView.swift
@@ -4,21 +4,59 @@
 //
 //  Created by Ignacio Tsai on 18/04/2024.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+
+            DayForecast(day: "Tue", isRainy: true,  high: 60, low: 40)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else{
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color{
+        if isRainy{
+            return Color.blue
+        } else{
+            return Color.yellow
+        }
+    }
+
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(.semibold)
+            Text("low: \(low)")
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
+}
+
