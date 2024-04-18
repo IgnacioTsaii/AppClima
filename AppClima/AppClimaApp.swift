9b1acec9609f89c634cf368804f81be4d7b555b9
Ignacio <igu99.tsai@gmail.com>
2024-04-18 10:36:03 -0300

    Initial Commit

---
+++ AppClima/AppClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClimaApp.swift
+//  AppClima
+//
+//  Created by Ignacio Tsai on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
